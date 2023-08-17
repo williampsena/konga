b59a5f1b8a1634af0b3484d6967f4351f0dadf2f
William Sena <william.sena@americanas.io>
2023-08-17 20:18:42 -0300

    fix cleanup

--- scripts/clean_packages.sh
+++ scripts/clean_packages.sh
@@ -3,22 +3,25 @@ database="${1:-all}"
 
 case "$database" in
 mongo)
-    npm uninstall sails-sqlserver sails-mysql sails-postgresql sails-disk
+    npm uninstall sails-disk
     ;;
 
 sqlserver)
-    npm uninstall sails-mongo sails-mysql sails-postgresql sails-disk
+    npm uninstall sails-disk
+    npm install sails-sqlserver
     ;;
 
 postgresql)
-    npm uninstall sails-mongo sails-sqlserver sails-mysql sails-disk
+    npm uninstall sails-disk
+    npm install sails-postgresql
     ;;
 
 mysql)
-    npm uninstall sails-mongo sails-sqlserver sails-postgresql sails-disk
+    npm uninstall sails-disk
+    npm install sails-mysql
     ;;
 disk)
-    npm uninstall sails-mongo sails-sqlserver sails-postgresql sails-mysql
+    npm uninstall sails-mongo
     ;;
 *)
     echo "no packages to cleanup"
